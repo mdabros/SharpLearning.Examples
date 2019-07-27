ac249fcd9980b89d57ea025850fb86c58d449a0d
mdabros <mdabros@users.noreply.github.com>
2019-07-27 18:25:55 +0200

    Update assembly version

--- src/SharpLearning.Examples.Integration/Properties/AssemblyInfo.cs
+++ src/SharpLearning.Examples.Integration/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("1f54bd04-7f1b-4577-bebf-e6cb123cd483")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
